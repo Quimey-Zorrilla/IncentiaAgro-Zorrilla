6e3fb752390e50eef9bfd2dab0302de3d215ffc0
Quimey-Zorrilla <zorrillaquimey@gmail.com>
2022-03-07 15:50:01 -0300

    Incentia-desafio4

--- src/componentes/ItemListContainer.js
+++ src/componentes/ItemListContainer.js
@@ -1,8 +1,17 @@
-function ItemListContainer(props) {
+import ItemCount from "./ItemCount";
+
+function ItemListContainer({greetings}) {
+
+    const n = 10;
+
     return (
         <>
         <main>
-            <h2>Bienvenido a {props.greeting}, esperamos que encuentre lo que desea!</h2>
+        <div>
+            {[...Array(n)].map((e, i) => <p key={i}>{greetings}</p>)}
+            <p>{greetings}</p>
+            <ItemCount onAdd={() => console.log('Agregado al carrito')} />
+        </div>
         </main>
         </>
     )
