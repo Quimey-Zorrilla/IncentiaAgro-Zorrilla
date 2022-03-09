0411f42d7d3a3e84f27f1323c1b67cb692b3efcb
Quimey-Zorrilla <zorrillaquimey@gmail.com>
2022-03-09 12:04:05 -0300

    desafio5

--- src/componentes/ItemListContainer.js
+++ src/componentes/ItemListContainer.js
@@ -1,4 +1,6 @@
 import ItemCount from "./ItemCount";
+import ItemList from './ItemList';
+import products from '../products.json';
 
 function ItemListContainer({greetings}) {
 
@@ -6,13 +8,17 @@ function ItemListContainer({greetings}) {
 
     return (
         <>
-        <main>
-        <div>
-            {[...Array(n)].map((e, i) => <p key={i}>{greetings}</p>)}
-            <p>{greetings}</p>
-            <ItemCount onAdd={() => console.log('Agregado al carrito')} />
-        </div>
-        </main>
+            <main>
+                <div>
+                    {[...Array(n)].map((e, i) => <p key={i}>{greetings}</p>)}
+                    <p>{greetings}</p>
+                    <ItemCount onAdd={() => console.log('Agregado al carrito')} />
+                </div>
+                <div>
+                    <h2>INCENTIA cuenta con amplia gama de productos organicos</h2>
+                    <ItemList list={products} />
+                </div>
+            </main>
         </>
     )
 }
