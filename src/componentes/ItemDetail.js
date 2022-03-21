a691a74dc2dc9afe80ef40d4f695ef5e827695b3
Quimey-Zorrilla <zorrillaquimey@gmail.com>
2022-03-21 12:18:16 -0300

    Incentia-Desafio8

--- src/componentes/ItemDetail.js
+++ src/componentes/ItemDetail.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import ItemCount from './ItemCount';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export const ItemDetail = ({
     id,
@@ -8,14 +11,29 @@ export const ItemDetail = ({
     descripcion,
     precio,
 }) => {
+    const [seleccionado, setSeleccionado] = useState(false)
+    const [agregar, setAgregar] = useState(false)
+
+    const onAdd = (unidadSeleccionada) => {
+        if(unidadSeleccionada !== undefined){
+            setSeleccionado(true)
+        }
+        setAgregar(true)
+    }
     return (
         <div className="detalles">
             <img src={image} alt={`${id}-${nombre}`} className="flex-col" />
-            <section>
-                <h1>{nombre}</h1>
-                <p>{descripcion}</p>
-                <h2>${precio}</h2>
-            </section>
+            <div>
+                <section>
+                    <h1>{nombre}</h1>
+                    <p>{descripcion}</p>
+                    <h2>${precio}</h2>
+                </section>
+                <section>
+                    <p>{seleccionado?"Compra realizada!":"No selecciono cantidad"}</p>
+                    {agregar?<Link to="/carrito/">Ir al carrito</Link> : <ItemCount initial={1} stock={5} onAdd={onAdd}/> }
+                </section>
+            </div>
         </div>
     );
 };
